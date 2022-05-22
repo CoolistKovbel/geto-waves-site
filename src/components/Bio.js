dd1720661bfd6f3135583bd2cd4eb1d296632ecf
coolistkovbel <coolistkovbel@gmail.com>
2022-05-22 15:08:07 -0400

    added styles

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,8 +2,31 @@ import React from 'react'
 
 function Bio() {
   return (
-    <div>
-      Bio
+    <div className='bio'>
+
+      <div className='img-container'>
+        <img src="https://i.pinimg.com/originals/69/f6/01/69f6010377ca4057293071ceea834c88.jpg" />
+      </div>
+
+      <div className='bio-content'>
+        <p>
+          Hey I am lyub. Just a dude trying to do some cool things I guess. Learning to be someone cool so I hope my girlfriend can relax and I can code more and more and who knows make something cool and out perform elon musk.
+        </p>
+        <p>
+          Soon To create more projects so... stay tuned..
+        </p>
+
+        <ul>
+          <li>NFT Collection</li>
+          <li>DEFI Application</li>
+          <li>Crypto Currency</li>
+        </ul>
+
+
+
+      </div>
+
+
     </div>
   )
 }
