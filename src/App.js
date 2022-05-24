1a375aef323554b11807b7eda6825b7fa7fbe434
coolistkovbel <coolistkovbel@gmail.com>
2022-05-23 23:13:12 -0400

    adding a few more features

--- src/App.js
+++ src/App.js
@@ -4,14 +4,35 @@ import { ethers } from "ethers";
 
 export default function App () {
 
-  const checkIfWalletIsConnect = () => {
-    // Make sure we have access to window.ethereum
-    const {ethereum} = window
+  // State Variables
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [validAccount, setValidAccount] = useState(false);
 
-    if(!ethereum){
-      alert("Get metmask")
-    } else {
-      console.log("Ethereum object exists: ", ethereum)
+
+  const checkIfWalletIsConnect = async () => {
+    try {
+      // Make sure we have access to window.ethereum
+      const {ethereum} = window
+
+      if(!ethereum){
+        alert("Get metamask")
+      } else {
+        console.log("Ethereum object exists: ", ethereum)
+      }
+
+      // Check to if authorized User
+      const accounts = await ethereum.request({method: "eth_accounts"});
+
+      if(accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Authorized account: ", account)
+        setValidAccount(true)
+        setCurrentAccount(account)
+      } else {
+        console.log("no authoized account")
+      }
+    } catch (err) {
+      console.log(err)
     }
 
   }
@@ -24,6 +45,9 @@ export default function App () {
   return (
     <div>
       <h2>Welcome to the Geto Wave App</h2>
+      <div>
+        <h2>Profile Account: {currentAccount}</h2>
+      </div>
       <button className="waveButton" onClick={null}>
           Wave at Me
       </button>
